d0d591daa0dece82966eeef868011641024d2cda
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-04-24 16:34:06 -0300

    Using HtmlWebpackPlugin in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const outputPath = path.resolve(__dirname, 'app/dist')//It gets the path since your system root until your current directory
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './app/src/js/app.js',//Entry point of this application
@@ -7,4 +8,10 @@ module.exports = {
         filename: 'bundle.js',//Here you can put any name you want
         path: outputPath//This is the location where the bundle is gonna be stored
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/src/app.html',
+            filename: 'app.html'
+        }),
+    ]
 };
